015a9423a3aedbf359c001ad269016196304ddcb
Binoy Samuel <82443302+BinoySamuelXBS@users.noreply.github.com>
2024-02-19 15:33:34 +0530

    agency registration added

--- xbs-care/src/App.js
+++ xbs-care/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login from './components/authentication/login';
 import ForgotPassword from './components/authentication/forgot-password';
+import RegistrationForm from './components/registration/agencyRegistration';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
      <Routes>
    <Route path="/" element={<Login/>} /> 
    <Route path="/forgot" element={<ForgotPassword/>} /> 
+   <Route path="/register" element={<RegistrationForm/>} /> 
    </Routes>
    </Router>
 
