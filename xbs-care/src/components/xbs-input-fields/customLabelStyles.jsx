9926e0a8ded4dcac54433a8c29873ad03a763ecd
Binoy Samuel <82443302+BinoySamuelXBS@users.noreply.github.com>
2024-02-26 12:13:38 +0530

    added Scroll into view with ref

--- xbs-care/src/components/xbs-input-fields/customLabelStyles.jsx
+++ xbs-care/src/components/xbs-input-fields/customLabelStyles.jsx
@@ -29,6 +29,16 @@ const styles = {
     marginBottom: '1rem',
     paddingTop: '2%',
   },
+  mediumTrad: {
+    color: '#194A9F',
+    fontSize: '15px',
+    fontWeight: 600,
+    lineHeight: 'normal',
+    width: '100%',
+    textAlign: 'left',
+    marginBottom: '1rem',
+    paddingTop: '6%',
+  },
   normal: {
     fontSize: '15px',
     fontWeight: 600,
